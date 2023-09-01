56afe5e99bd4d221dda0893a30c18cbf1b3d7046
Mariano Montone <marianomontone@gmail.com>
2023-09-01 14:52:58 -0300

    Adjustment to refs in json-schemas

--- json-schema.lisp
+++ json-schema.lisp
@@ -41,8 +41,7 @@
 
 (defun parse-json-schema-ref (ref)
   (let ((schema-name (car (last (split-sequence:split-sequence #\/ ref)))))
-    (make-instance 'schema-reference-schema
-                   :schema-name (intern (json::simplified-camel-case-to-lisp schema-name)))))
+    (list 'ref (intern (json::simplified-camel-case-to-lisp schema-name)))))
 
 (defun schema-from-json-schema (json-schema)
   "Create a SCHEMA from JSON-SCHEMA.
