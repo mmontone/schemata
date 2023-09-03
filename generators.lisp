dbc1ff57b1e49ba53b43aa249ea51102756efd2c
Mariano Montone <marianomontone@gmail.com>
2023-09-03 18:45:15 -0300

    Generators

--- generators.lisp
+++ generators.lisp
@@ -3,29 +3,10 @@
 
 (in-package :schemata-generators)
 
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (def-genex-macro maybe (form)
-    `(or nil ,form))
-
-  ;; Doesn't work because it caches the call func
-  ;; (def-generator func (func)
-  ;;   (generator (funcall func)))
-
-  (defclass func (check-it::custom-generator)
-    ((check-it::bias :initform 1.0
-                     :accessor check-it::bias
-                     :allocation :class)
-     (func :initarg :func)))
-  (setf (get 'func 'check-it::genex-type) 'generator)
-  (setf (get 'func 'check-it::generator-form)
-        `(lambda ,'(func) (make-instance ','func ,@'(:func func))))
-
-  (defmethod generate ((generator func))
-    (let ((func (slot-value generator 'func)))
-      (funcall func)))
-  )
-
-;;(generate (generator (func (lambda () 22))))
+;; check-it patch
+;; Allow functions as generators
+(defmethod check-it::generate ((generator function))
+  (funcall generator))
 
 (defgeneric generator-for-schema (schema))
 (defgeneric generator-for-type (type type-schema))
@@ -44,9 +25,9 @@
 
 (defun attribute-generator (attribute)
   (let ((assoc-generator
-          (generator (func (lambda ()
-                             (cons (attribute-name attribute)
-                                   (generate (generator-for-schema (attribute-type attribute)))))))))
+          (lambda ()
+            (cons (attribute-name attribute)
+                  (generate (generator-for-schema (attribute-type attribute)))))))
     (if (attribute-required-p attribute)
         assoc-generator
         (generator
@@ -54,12 +35,11 @@
                 (generator (maybe gen)))))))
 
 (defmethod generator-for-schema ((schema object-schema))
-  (generator
-   (func (lambda ()
-           (remove nil
-                   (mapcar #'generate
-                           (loop for attribute in (object-attributes schema)
-                                 collect (attribute-generator attribute))))))))
+  (lambda ()
+    (remove nil
+            (mapcar #'generate
+                    (loop for attribute in (object-attributes schema)
+                          collect (attribute-generator attribute))))))
 
 (defmethod generator-for-schema ((schema schema-reference-schema))
   (schemata::referenced-schema schema))
